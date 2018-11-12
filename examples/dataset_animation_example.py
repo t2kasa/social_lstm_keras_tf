855c879db6ef76a676c720543d598e8b8755426b
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-11-12 23:54:23 +0900

    animation example of ground truth trajectories

---
+++ examples/dataset_animation_example.py
@@ -0,0 +1,48 @@
+import matplotlib
+
+# to show animation in PyCharm
+matplotlib.use('Qt5Agg')  # NOQA
+
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.animation import FuncAnimation
+from general_utils import DatasetKind
+from preprocessors.eth_dataset_preprocessor import EthDatasetPreprosessor
+
+
+class TrajectoryAnimator:
+    def __init__(self, df):
+        self.df = df
+        self.fig, self.axes = plt.subplots(1, 1)
+        self.frames = self.df['frame'].unique()
+
+    def update(self, t):
+        self.axes.clear()
+
+        frame_range = self.frames[np.maximum(t - 10, 0):t]
+        target_df = self.df[self.df['frame'].isin(frame_range)]
+        print(np.unique(target_df['frame']))
+
+        for i, df_i in target_df.groupby('id'):
+            xy = np.array(df_i[['x', 'y']])
+            print(xy)
+            self.axes.plot(xy[:, 0], xy[:, 1])
+
+        return self.axes,
+
+
+def main():
+    hotel_data_dir = Path(Path(__file__).parent / 'data/datasets/eth/hotel')
+    preprocessor = EthDatasetPreprosessor(hotel_data_dir, DatasetKind.hotel)
+    df = preprocessor.preprocess_frame_data()
+
+    animator = TrajectoryAnimator(df)
+    ani = FuncAnimation(animator.fig, animator.update, interval=33)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
