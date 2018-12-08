d7f7d1beed542c367cfe53f691fecce007aa0fce
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-12-08 17:45:27 +0900

    refactoring

--- preprocessors/eth_dataset_preprocessor.py
+++ preprocessors/eth_dataset_preprocessor.py
@@ -11,33 +11,26 @@ class EthDatasetPreprosessor:
 
     def __init__(self, data_dir, dataset_kind):
         self.image_size = get_image_size(dataset_kind)
-
         # read homography matrix
-        homography_file = os.path.join(data_dir, "H.txt")
-        self.homography = np.genfromtxt(homography_file)
-
+        self.homography = np.genfromtxt(os.path.join(data_dir, "H.txt"))
         # read trajectory data
-        obsmat_file = os.path.join(data_dir, "obsmat.txt")
-        obs_columns = ["frame", "id", "px", "pz", "py", "vx", "vz", "vy"]
-        obs_df = pd.DataFrame(np.genfromtxt(obsmat_file), columns=obs_columns)
-        # remain only (frame index, pedestrian id, position x, position y)
-        self.raw_df = obs_df[["frame", "id", "px", "py"]]
+        self.raw_pos_df = _load_obsmat_file(data_dir)
 
     def preprocess_frame_data(self):
         # position preprocessing
-        xy = np.array(self.raw_df[["px", "py"]])
+        xy = np.array(self.raw_pos_df[["px", "py"]])
         # world xy to image xy: inverse mapping of homography
         xy = self._world_to_image_xy(xy, self.homography)
         # normalize
         xy = xy / self.image_size
 
-        pos_df = pd.DataFrame({
-            "frame": self.raw_df["frame"],
-            "id": self.raw_df["id"],
+        preprocessed_df = pd.DataFrame({
+            "frame": self.raw_pos_df["frame"],
+            "id": self.raw_pos_df["id"],
             "x": xy[:, 0],
             "y": xy[:, 1]
         })
-        return pos_df
+        return preprocessed_df
 
     @staticmethod
     def _world_to_image_xy(world_xy, homography):
@@ -55,3 +48,11 @@ class EthDatasetPreprosessor:
         image_xy1 = np.linalg.inv(homography).dot(world_xy1.T).T
         image_xy = image_xy1[:, :2] / np.expand_dims(image_xy1[:, 2], axis=1)
         return image_xy
+
+
+def _load_obsmat_file(data_dir):
+    obsmat_file = os.path.join(data_dir, "obsmat.txt")
+    obs_columns = ["frame", "id", "px", "pz", "py", "vx", "vz", "vy"]
+    obs_df = pd.DataFrame(np.genfromtxt(obsmat_file), columns=obs_columns)
+    pos_df = obs_df[["frame", "id", "px", "py"]]
+    return pos_df
