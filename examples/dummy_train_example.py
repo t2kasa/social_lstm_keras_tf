2cd2bf5dd39ada96184a13ca0f5127767e20133c
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-12-08 11:41:08 +0900

    rename variable

--- examples/dummy_train_example.py
+++ examples/dummy_train_example.py
@@ -8,7 +8,7 @@ from my_social_model_tfe import SocialLSTM
 if __name__ == '__main__':
     tf.enable_eager_execution()
     args = Namespace(obs_len=3, pred_len=2, max_n_peds=52, pxy_dim=3,
-                     cell_side=0.5, n_side_cells=4, n_states=32, emb_dim=16,
+                     cell_side=0.5, n_side_cells=4, lstm_dim=32, emb_dim=16,
                      out_dim=5)
 
     # my implementation works only when the batch size equals to 1.
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     x_input = tf.convert_to_tensor(x_input, dtype=tf.float32)
 
     social_lstm = SocialLSTM(args.pred_len, args.cell_side, args.n_side_cells,
-                             args.n_states, args.emb_dim, args.out_dim)
+                             args.lstm_dim, args.emb_dim, args.out_dim)
     social_lstm.call(x_input)
     print('passed!')
     exit(0)
