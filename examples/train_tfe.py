80d251bead17e239bcc515ab9b72f44b076e143b
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-12-17 23:13:49 +0900

    change the return value of load_single_dataset function.

--- examples/train_tfe.py
+++ examples/train_tfe.py
@@ -1,9 +1,8 @@
 from argparse import ArgumentParser
-from pathlib import Path
 
 import tensorflow as tf
 
-from commons.general_utils import pxy_dim, out_dim
+from datasets.load_single_dataset import load_single_dataset
 from social_lstm.my_social_model_tfe import SocialLSTM
 
 
@@ -11,7 +10,6 @@ def load_args():
     parser = ArgumentParser()
     # train params
     parser.add_argument('--n_epochs', type=int, required=True)
-    parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--learning_rate', type=float, default=0.003)
 
     # model params
@@ -31,37 +29,26 @@ def load_args():
     return args
 
 
-def _load_single_dataset(data_dir, args):
-    from datasets.build_dataset import build_dataset
-
-    dataset_name = Path(data_dir).stem
-    if dataset_name == 'hotel':
-        image_size = (750, 576)
-    else:  # eth
-        image_size = (640, 480)
-
-    obs_true_seqs, pred_true_seqs = build_dataset(data_dir, image_size,
-                                                  args.obs_len, args.pred_len)
-    return obs_true_seqs, pred_true_seqs
-
-
 def main():
     tf.enable_eager_execution()
     args = load_args()
 
-    # train_datasets = [_load_single_dataset(d, args) for d in
-    #                   args.train_data_dirs]
+    train_ds_list = [load_single_dataset(d, args) for d in args.train_data_dirs]
+    # test_ds_list = [load_single_dataset(d, args) for d in args.test_data_dirs]
+
     # test_datasets = [_load_single_dataset(d, args) for d in args.test_data_dirs]
 
-    # xs, ys = train_datasets[0]
-    # test_ds = test_datasets[0]
-    # print(tf.shape(xs[0]))
-    # exit(0)
+    x_ds, y_ds = train_ds_list[0]
 
-    xs = [tf.random.normal([7, 4, 2])]
+    # test_ds = test_datasets[0]
+
+    # xs = [tf.random.normal([7, 4, 2])]
     social_lstm = SocialLSTM(args.pred_len, args.cell_side, args.n_side_cells,
                              args.lstm_dim, args.emb_dim)
-    social_lstm.call(xs)
+
+    for i, x in enumerate(x_ds):
+        print(i)
+        social_lstm.call(x)
 
 
 if __name__ == '__main__':
