e4c8303b828085b563908f1f9e2b342fcfaaa1f2
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-12-24 18:58:56 +0900

    add arrange process to datasets

--- examples/prepare_dataset.py
+++ examples/prepare_dataset.py
@@ -1,8 +1,8 @@
 import os
 
-from datasets.utils.download_datasets import download_datasets
+from datasets.utils.download_datasets import download_and_arrange_datasets
 
 if __name__ == '__main__':
     here = os.path.abspath(os.path.dirname(__file__))
     data_root_dir = os.path.join(here, '../data')
-    download_datasets(data_root_dir)
+    download_and_arrange_datasets(data_root_dir)
