24eac120b934baa47980ee249d83d9797a08d49c
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-12-25 04:01:59 +0900

    add evaluation function

--- examples/train_tfe.py
+++ examples/train_tfe.py
@@ -36,7 +36,7 @@ def load_args():
     return args
 
 
-def train(social_lstm, optimizer, train_ds, step_counter, log_interval=5):
+def train_epoch(social_lstm, optimizer, train_ds, step_counter, log_interval=5):
     for batch_index, (pos_true_obs, pos_true_pred) in enumerate(train_ds):
         with tf.contrib.summary.record_summaries_every_n_global_steps(
                 log_interval, global_step=step_counter):
@@ -60,6 +60,24 @@ def train(social_lstm, optimizer, train_ds, step_counter, log_interval=5):
                       f'abe: {abe}')
 
 
+def eval_epoch(social_lstm, test_ds):
+    loss_mean = tf.contrib.eager.metrics.Mean('loss', tf.float32)
+    abe_mean = tf.contrib.eager.metrics.Mean('abe', tf.float32)
+
+    for pos_true_obs, pos_true_pred in test_ds:
+        o_pred = social_lstm(pos_true_obs)
+        loss_mean(compute_loss(o_pred, pos_true_pred))
+
+        pos_true_pred = tf.reshape(pos_true_pred, [-1, 2])
+        pos_pred_pred = normal2d_sample(o_pred)
+        abe_mean(compute_abe_tf(pos_true_pred, pos_pred_pred))
+
+    print(f'test loss: {loss_mean.result()}, abe: {abe_mean.result()}')
+    with tf.contrib.summary.always_record_summaries():
+        tf.contrib.summary.scalar('loss', loss_mean.result())
+        tf.contrib.summary.scalar('abe', abe_mean.result())
+
+
 def main():
     tf.enable_eager_execution()
     args = load_args()
@@ -72,12 +90,12 @@ def main():
 
     train_dir = str(Path(args.out_dir, 'train'))
     test_dir = str(Path(args.out_dir, 'test'))
-    Path(args.out_dir).mkdir(parents=True, exist_ok=True)
+    tf.gfile.MakeDirs(args.out_dir)
 
     train_summary_writer = tf.contrib.summary.create_file_writer(
         train_dir, name='train')
     test_summary_writer = tf.contrib.summary.create_file_writer(
-        test_dir, name='train')
+        test_dir, name='test')
 
     step_counter = tf.train.get_or_create_global_step()
 
@@ -86,7 +104,9 @@ def main():
 
     for _ in range(args.n_epochs):
         with train_summary_writer.as_default():
-            train(social_lstm, optimizer, train_ds, step_counter)
+            train_epoch(social_lstm, optimizer, train_ds.take(11), step_counter)
+        with test_summary_writer.as_default():
+            eval_epoch(social_lstm, test_ds.take(11))
 
 
 if __name__ == '__main__':
