faf7cea6482133cfd068033e4e7754c1633ae18e
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-12-24 21:42:05 +0900

    move

--- preprocessors/preprocess_ucy.py
+++ preprocessors/preprocess_ucy.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
+from preprocessors.preprocessors_utils import interpolate_pos_df
+from preprocessors.preprocessors_utils import thin_out_pos_df
+
 
 def preprocess_ucy(data_dir):
     return UcyPreprocessor(data_dir).preprocess_frame_data()
@@ -94,34 +97,3 @@ class UcyPreprocessor:
             'y': xy[:, 1]
         })
         return pos_df_norm
-
-
-def thin_out_pos_df(pos_df, interval):
-    all_frames = pos_df['frame'].unique()
-    remained_frames = np.arange(all_frames[0], all_frames[-1] + 1, interval)
-    remained_rows = pos_df['frame'].isin(remained_frames)
-
-    pos_df_thin = pos_df[remained_rows]
-    pos_df_thin = pos_df_thin.reset_index(drop=True)
-    return pos_df_thin
-
-
-def interpolate_pos_df(pos_df):
-    pos_df_interp = []
-
-    for pid, pid_df in pos_df.groupby('id'):
-        observed_frames = np.array(pid_df['frame'])
-        frame_range = np.arange(observed_frames[0], observed_frames[-1] + 1)
-
-        x_interp = np.interp(frame_range, pid_df['frame'], pid_df['x'])
-        y_interp = np.interp(frame_range, pid_df['frame'], pid_df['y'])
-
-        pos_df_interp.append(pd.DataFrame({
-            'frame': frame_range,
-            'id': pid,
-            'x': x_interp,
-            'y': y_interp
-        }))
-
-    pos_df_interp = pd.concat(pos_df_interp)
-    return pos_df_interp
--- preprocessors/preprocessors_utils.py
+++ preprocessors/preprocessors_utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 from commons.general_utils import DatasetKind, _check_dataset_kind
 from preprocessors.preprocess_ewap import EwapPreprocessor
 from preprocessors.preprocess_ucy import UcyPreprocessor
@@ -13,3 +16,34 @@ def create_dataset_preprocessor(data_dir, dataset_kind):
         return UcyPreprocessor(data_dir, dataset_kind)
 
     raise ValueError("dataset_kind")
+
+
+def thin_out_pos_df(pos_df, interval):
+    all_frames = pos_df['frame'].unique()
+    remained_frames = np.arange(all_frames[0], all_frames[-1] + 1, interval)
+    remained_rows = pos_df['frame'].isin(remained_frames)
+
+    pos_df_thin = pos_df[remained_rows]
+    pos_df_thin = pos_df_thin.reset_index(drop=True)
+    return pos_df_thin
+
+
+def interpolate_pos_df(pos_df):
+    pos_df_interp = []
+
+    for pid, pid_df in pos_df.groupby('id'):
+        observed_frames = np.array(pid_df['frame'])
+        frame_range = np.arange(observed_frames[0], observed_frames[-1] + 1)
+
+        x_interp = np.interp(frame_range, pid_df['frame'], pid_df['x'])
+        y_interp = np.interp(frame_range, pid_df['frame'], pid_df['y'])
+
+        pos_df_interp.append(pd.DataFrame({
+            'frame': frame_range,
+            'id': pid,
+            'x': x_interp,
+            'y': y_interp
+        }))
+
+    pos_df_interp = pd.concat(pos_df_interp)
+    return pos_df_interp
