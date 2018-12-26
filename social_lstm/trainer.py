3769e0f070d03e2931e34abd4b3c4a6f25f753e4
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-12-27 01:01:06 +0900

    tentative commit

--- social_lstm/trainer.py
+++ social_lstm/trainer.py
@@ -15,23 +15,34 @@ class Trainer:
         self.test_ds = test_ds
         self.n_epochs = n_epochs
 
-        train_dir = str(Path(out_dir, 'train'))
-        test_dir = str(Path(out_dir, 'test'))
-        tf.gfile.MakeDirs(str(out_dir))
+        # create summaries
+        train_dir = Path(out_dir, 'train').as_posix()
+        test_dir = Path(out_dir, 'test').as_posix()
         self.train_summary_writer = tf.contrib.summary.create_file_writer(
             train_dir, name='train')
         self.test_summary_writer = tf.contrib.summary.create_file_writer(
             test_dir, name='test')
 
+        # create checkpoint
+        model_dir = Path(out_dir, 'checkpoints/').as_posix()
+        self.checkpoint_prefix = Path(model_dir, 'ckpt').as_posix()
         self.step_counter = tf.train.get_or_create_global_step()
+        self.checkpoint = tf.train.Checkpoint(
+            model=model, optimizer=optimizer, step_counter=self.step_counter)
+        self.checkpoint.restore(tf.train.latest_checkpoint(model_dir))
+
+        Path(out_dir).mkdir(parents=True, exist_ok=True)
 
     def run(self):
-        for epoch in range(self.n_epochs):
-            print(f'epoch: {epoch:03d}')
-            with self.train_summary_writer.as_default():
-                self.train_epoch()
-            with self.test_summary_writer.as_default():
-                self.eval_epoch()
+        with tf.device('/gpu:0'):
+            for epoch in range(self.n_epochs):
+                print(f'epoch: {epoch:03d}')
+                with self.train_summary_writer.as_default():
+                    self.train_epoch()
+                with self.test_summary_writer.as_default():
+                    self.eval_epoch()
+
+                self.checkpoint.save(self.checkpoint_prefix)
 
     def train_epoch(self, log_interval=5):
         for index, (pos_obs_true, pos_future_true) in enumerate(self.train_ds):
@@ -39,7 +50,7 @@ class Trainer:
                     log_interval, global_step=self.step_counter):
                 with tf.GradientTape() as tape:
                     o_future_pred = self.model(pos_obs_true)
-                    loss_value = compute_loss(o_future_pred, pos_future_true)
+                    loss_value = compute_loss(pos_future_true, o_future_pred)
 
                 pos_future_true = tf.reshape(pos_future_true, [-1, 2])
                 pos_future_pred = normal2d_sample(o_future_pred)
@@ -63,7 +74,7 @@ class Trainer:
 
         for pos_obs_true, pos_future_true in self.test_ds:
             o_future_pred = self.model(pos_obs_true)
-            loss_mean(compute_loss(o_future_pred, pos_future_true))
+            loss_mean(compute_loss(pos_future_true, o_future_pred))
 
             pos_future_true = tf.reshape(pos_future_true, [-1, 2])
             pos_future_pred = normal2d_sample(o_future_pred)
