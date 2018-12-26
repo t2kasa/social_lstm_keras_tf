8a4f417e71f74a6a56ce06f6d2c5d5faf9d1e5e1
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-12-27 00:40:47 +0900

    delete legacy parts

--- datasets/data_utils.py
+++
@@ -1,5 +0,0 @@
-def obs_pred_split(obs_len, pred_len, *arrays):
-    obs_len_arrays = [a[:, :obs_len, ...] for a in arrays]
-    pred_len_arrays = [a[:, obs_len:obs_len + pred_len, ...] for a in arrays]
-
-    return obs_len_arrays, pred_len_arrays
